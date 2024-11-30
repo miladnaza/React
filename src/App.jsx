f5ba3f620309965c10408edb80db3dceac6c340b
AnshSatiish <169073333+AnshSatiish@users.noreply.github.com>
2024-11-30 18:44:22 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import Try from "./pages/Try";
+import Details from "./pages/Details";
 import AccountCenter from "./components/AccountCenter";
 import Bookslist from "./components/Bookslist";
 import "./styles/ShoppingCart.css";
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/AccountCenter" element={<AccountCenter />} />
+         <Route path="/book-details/:shortTitle" element={<Details />} />
         <Route path="/Bookslist" element={<Bookslist />} />
         <Route path="/try" element={<Try />} />
       </Routes>
